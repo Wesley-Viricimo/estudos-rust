1873ce66841561f0e98e2bae38def3af9ac7e1ee
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-11-17 22:25:08 -0300

    Iniciando com variáveis

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\wesle\Documents\Projetos\rust\hello\target\debug\deps\hello.exe: src/main.rs
+
+C:\Users\wesle\Documents\Projetos\rust\hello\target\debug\deps\hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\wesle\Documents\Projetos\rust\hello\target\debug\hello.exe: C:\Users\wesle\Documents\Projetos\rust\hello\src\main.rs
