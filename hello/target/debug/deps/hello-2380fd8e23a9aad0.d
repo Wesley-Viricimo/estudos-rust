1409fd75ef64deaf10afaed30254b9113bd0e9ad
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-11-17 22:07:47 -0300

    Iniciando estudos com rust

---
+++ hello/target/debug/deps/hello-2380fd8e23a9aad0.d
@@ -0,0 +1,5 @@
+C:\Users\wesle\Documents\Projetos\rust\hello\target\debug\deps\libhello-2380fd8e23a9aad0.rmeta: src/main.rs
+
+C:\Users\wesle\Documents\Projetos\rust\hello\target\debug\deps\hello-2380fd8e23a9aad0.d: src/main.rs
+
+src/main.rs:
